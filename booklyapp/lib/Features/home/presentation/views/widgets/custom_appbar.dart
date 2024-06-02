74b531aaea5a8097b5cc0d18f4f6e9dc7220e615
Your Name <alqisyabdallah@gmail.com>
2024-06-02 19:46:48 +0300

    add featured list view to home

--- booklyapp/lib/Features/home/presentation/views/widgets/custom_appbar.dart
+++ booklyapp/lib/Features/home/presentation/views/widgets/custom_appbar.dart
@@ -8,7 +8,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
+      padding: const EdgeInsets.only(top: 40, bottom: 20, left: 24, right: 24),
       child: Row(
         children: [
           Image.asset(
--- booklyapp/lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
+++ booklyapp/lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -6,17 +6,14 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .28,
-      child: AspectRatio(
-        aspectRatio: 2.8 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(AssetsData.test),
-            ),
+    return AspectRatio(
+      aspectRatio: 2.8 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.test),
           ),
         ),
       ),
