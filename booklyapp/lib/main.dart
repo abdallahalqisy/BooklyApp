d74dadb9769c7a1d3223370f9114c247797152ea
Your Name <alqisyabdallah@gmail.com>
2024-06-02 20:25:49 +0300

    add font family to ourapp

--- booklyapp/lib/main.dart
+++ booklyapp/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:booklyapp/Features/splash/presentation/views/splash_view.dart';
 import 'package:booklyapp/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const Bookly());
@@ -16,8 +17,9 @@ class Bookly extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-      ),
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
       home: const SplashView(),
     );
   }
