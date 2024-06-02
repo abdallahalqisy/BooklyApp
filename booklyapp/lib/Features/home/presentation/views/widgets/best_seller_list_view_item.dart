0dc51244cd714c94da1e0fae81eddc1c9335a5a4
Your Name <alqisyabdallah@gmail.com>
2024-06-02 23:11:08 +0300

    best seller item done

---
+++ booklyapp/lib/Features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,76 @@
+import 'package:booklyapp/Features/home/presentation/views/widgets/book_rating.dart';
+import 'package:booklyapp/constants.dart';
+import 'package:booklyapp/core/utils/assets.dart';
+import 'package:booklyapp/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 30),
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(AssetsData.test1),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 30,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * .5,
+                    child: Text(
+                      'Harry Potter and the Goblet of Fire',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.textStyle20
+                          .copyWith(fontFamily: kGtSectrafine),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  const Text(
+                    'J.K. Rowling',
+                    style: Styles.textStyle14,
+                  ),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '19.99 €',
+                        style: Styles.textStyle20
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      const BookRating(),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- booklyapp/lib/core/utils/styles.dart
+++ booklyapp/lib/core/utils/styles.dart
@@ -1,11 +1,12 @@
-import 'package:booklyapp/constants.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
   static const textStyle18 =
       TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
   static const textStyle20 = TextStyle(
-      fontSize: 20, fontWeight: FontWeight.normal, fontFamily: kGtSectrafine);
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
   static const textStyle14 =
       TextStyle(fontSize: 14, fontWeight: FontWeight.normal);
   static const textStyle16 =
