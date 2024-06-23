2cb98a4806f731df5a9b6617fbeb5d9d0b5d64ba
Your Name <alqisyabdallah@gmail.com>
2024-06-23 13:42:38 +0300

    create books action

--- booklyapp/lib/Features/home/presentation/views/widgets/book_view_details_body.dart
+++ booklyapp/lib/Features/home/presentation/views/widgets/book_view_details_body.dart
@@ -1,7 +1,9 @@
 import 'package:booklyapp/Features/home/presentation/views/widgets/book_rating.dart';
+import 'package:booklyapp/Features/home/presentation/views/widgets/books_action.dart';
 import 'package:booklyapp/Features/home/presentation/views/widgets/custom_book_details_appbar.dart';
 import 'package:booklyapp/Features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:booklyapp/core/utils/styles.dart';
+import 'package:booklyapp/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -44,7 +46,11 @@ class BookDetailsViewBody extends StatelessWidget {
           ),
           const BookRating(
             mainAxisAlignment: MainAxisAlignment.center,
-          )
+          ),
+          const SizedBox(
+            height: 37,
+          ),
+          const BooksAction()
         ],
       ),
     );
