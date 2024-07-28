408d79c7706d612deb7b6d68bf6125d19c2ea776
Your Name <alqisyabdallah@gmail.com>
2024-07-28 12:38:04 +0300

    cached network image

--- booklyapp/lib/Features/home/presentation/views/widgets/custom_book_item.dart
+++ booklyapp/lib/Features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.imagUrl});
@@ -7,13 +8,15 @@ class CustomBookImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 16),
-      child: AspectRatio(
-        aspectRatio: 2.6 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image:
-                DecorationImage(fit: BoxFit.fill, image: NetworkImage(imagUrl)),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: AspectRatio(
+          aspectRatio: 2.6 / 4,
+          child: CachedNetworkImage(
+            imageUrl: imagUrl,
+            fit: BoxFit.fill,
+            errorWidget: (context, url, error) =>
+                const Icon(Icons.error_outline_sharp),
           ),
         ),
       ),
